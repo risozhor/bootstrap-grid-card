37594efed6dcb9d51d900d565b125990a7bf0a1f
Shytyi Oleksandr <hitweb@gmail.com>
2024-02-24 16:21:45 +0200

    Update style.js

--- src/style.js
+++ src/style.js
@@ -1,5 +1,5 @@
 import { css } from 'lit';
-import bootstrapGridStyle from 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrapGridStyle from 'bootstrap/dist/css/bootstrap.css';
 
 const style = [bootstrapGridStyle, css`
     :host {
